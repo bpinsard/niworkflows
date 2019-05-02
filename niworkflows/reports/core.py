9b62278de5a596e1522f2019dfbdd6ac0b4e9c90
oesteban <code@oscaresteban.es>
2019-05-02 14:33:52 -0700

    fix(reports): improve reportlets naming and visualization of ICA-AROMA

--- niworkflows/reports/core.py
+++ niworkflows/reports/core.py
@@ -20,7 +20,7 @@ from nipype.utils.filemanip import copyfile
 
 
 PLURAL_SUFFIX = defaultdict(str('s').format, [('echo', 'es')])
-SVG_SNIPPET = """\
+SVG_SNIPPET = ["""\
 <object class="svg-reportlet" type="image/svg+xml" data="./{0}">
 Problem loading figure {0}. If the link below works, please try \
 reloading the report in your browser.</object>
@@ -28,7 +28,13 @@ reloading the report in your browser.</object>
 <div class="elem-filename">
     Get figure file: <a href="./{0}" target="_blank">{0}</a>
 </div>
-"""
+""", """\
+<img class="svg-reportlet" src="./{0}" style="width: 100%" />
+</div>
+<div class="elem-filename">
+    Get figure file: <a href="./{0}" target="_blank">{0}</a>
+</div>
+"""]
 
 
 class Element(object):
@@ -145,7 +151,19 @@ class Reportlet(Element):
                 out_file = out_dir / linked_svg
                 out_file.parent.mkdir(parents=True, exist_ok=True)
                 copyfile(src, out_file, copy=True, use_hardlink=True)
-                contents = SVG_SNIPPET.format(linked_svg)
+                is_static = config.get('static', True)
+                contents = SVG_SNIPPET[is_static].format(linked_svg)
+
+                if not is_static:
+                    # Remove height and width attributes from initial <svg> tag
+                    svglines = out_file.read_text().splitlines()
+                    expr = re.compile(r' (height|width)=["\'][0-9]+(\.[0-9]*)?[a-z]*["\']')
+                    for l, line in enumerate(svglines[:6]):
+                        if line.strip().startswith('<svg'):
+                            newline = expr.sub('', line)
+                            svglines[l] = newline
+                            out_file.write_text('\n'.join(svglines))
+                            break
 
             if contents:
                 self.components.append((contents, desc_text))
